f1eecbdb22cf750e0998f3e39c427a76e9412b23
Bader Alsabah <bader@badercodes.com>
2022-06-30 12:21:35 +0400

    added a landing page route for testing

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require("express");
 const server = express();
 
+// adding an index / landing page route to server
+server.get("/", function (req, res) {
+  res.send("<h1>You have successfully landed on the Dubai Marketpalce!</h1>");
+});
+
 // server is listening to requests on port 3000
 server.listen(3000, function () {
   console.log("Server is running and listening on port 3000");
